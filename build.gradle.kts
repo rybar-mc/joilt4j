cdbb053a8aa163364842c6532ddd75cadb0d6b64
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-03-08 21:35:50 +0100

    build: publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java-library")
+    id("maven-publish")
 }
 
 group = "org.rybar.joilt4j"
@@ -25,11 +26,46 @@ tasks.withType<Test> {
 }
 
 java {
+    withSourcesJar()
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
 
 tasks {
     withType<JavaCompile> {
         options.encoding = "UTF-8"
+    }
+}
+
+tasks.withType<Zip> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "JopgaRepo"
+            url = uri("https://repo.jopga.me/private")
+            credentials {
+                username = project.findProperty("jopga.user").toString()
+                password = project.findProperty("jopga.key").toString()
+            }
+        }
+    }
+
+    publications.create<MavenPublication>("maven") {
+        groupId = project.group as String
+        artifactId = "core"
+        version = project.version as String
+
+        pom {
+            name.set("Joilt4j")
+            description.set("A lightweight, zero-dependency logging library")
+        }
+
+        artifact(tasks.named("jar")) {
+            classifier = ""
+        }
+
+        artifact(tasks.named("sourcesJar"))
     }
 }
